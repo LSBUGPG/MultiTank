dc6256e1246fb51b7a4994cb58193e64e86cbb7b
Paul Sinnett <paul.sinnett@gmail.com>
2018-03-20 09:24:36 +0000

    customize UI

---
+++ Assets/Lobby.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Lobby : MonoBehaviour
+{
+    public GameObject connectionOptions;
+    public LobbyScreen lobbyScreen;
+    public GameObject enterAddressScreen;
+    NetworkLobbyManager networkLobbyManager;
+    NetworkClient networkClient;
+    List<GameObject> screens;
+
+    void Awake()
+    {
+        networkLobbyManager = GetComponent<NetworkLobbyManager>();
+        screens = new List<GameObject>();
+        screens.Add(connectionOptions);
+        screens.Add(lobbyScreen.gameObject);
+        screens.Add(enterAddressScreen);
+
+        ChangeScreen(connectionOptions);
+    }
+
+    public void ChangeScreen(GameObject newScreen)
+    {
+        foreach (var screen in screens)
+        {
+            if (screen != newScreen)
+            {
+                screen.SetActive(false);
+            }
+        }
+        newScreen.SetActive(true);
+    }
+
+    public void Host()
+    {
+        networkClient = networkLobbyManager.StartHost();
+        ChangeScreen(lobbyScreen.gameObject);
+        lobbyScreen.SetTitle("Host lobby");
+    }
+
+    public void EnterAddress()
+    {
+        ChangeScreen(enterAddressScreen);
+    }
+
+    public void Join()
+    {
+        networkClient = networkLobbyManager.StartClient();
+        ChangeScreen(lobbyScreen.gameObject);
+        lobbyScreen.SetTitle("Player lobby");
+    }
+
+    public void Disconnect()
+    {
+        networkClient = null;
+        networkLobbyManager.StopHost();
+        ChangeScreen(connectionOptions);
+    }
+}
---
+++ Assets/LobbyScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyScreen : MonoBehaviour
+{
+    public Text title;
+
+    public void SetTitle(string title)
+    {
+        this.title.text = title;
+    }
+}
